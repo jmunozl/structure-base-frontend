5d2199f6a96b5717a1a84dc832e20b9f67869c4b
jmunozl <jos.munozl@gmail.com>
2022-03-30 13:30:55 -0300

    Cors api

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+	const API_URL = 'https://dashboard.heroku.com/apps/structure-base-backend'
 	const [count, setCount] = useState(0)
+	const [messages, setMessages] = useState('')
+
+	const fetchData = async () => {
+		const response = await axios.get(API_URL)
+		console.log(response)
+		setMessages(response.data.message)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
 
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hello Vite + React!</p>
+				<p> {messages} Vite + React!</p>
 				<p>
 					<button type='button' onClick={() => setCount(count => count + 1)}>
 						count is: {count}
